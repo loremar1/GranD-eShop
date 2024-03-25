394b2235a0d6f9383eb456e51e12db3928aca4fa
loremar1 <lorenasanchezmar4gmail.com>
2024-03-25 11:56:16 -0600

    Agregar archivos de proyecto.

---
+++ wwwroot/js/Login.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('input[type="radio"]').click(function () {
+        if ($(this).attr('id') == 'check') {
+            $('#content').show();
+            $('#content2').hide(); // Oculta el contenido2 si se selecciona el botón check
+        }
+        else if ($(this).attr('id') == 'check1') {
+            $('#content2').show();
+            $('#content').hide(); // Oculta el contenido si se selecciona el botón check1
+        }
+    });
+});
